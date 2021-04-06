92475c6dd271bc717e2f7a499dbea355834d62cd
EC2 Default User <ec2-user@ip-172-31-21-95.ap-northeast-1.compute.internal>
2021-04-06 18:14:06 +0000

    set up bootstrap4 cdn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+ #他のコントローラやリソース設定かくにん必要
+ resources :comments
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
