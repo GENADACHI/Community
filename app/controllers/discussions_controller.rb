8487e0cca4c30c9e26e4b2978cfcb7878646013d
GENADACHI <adachigen46@gmail.com>
2021-04-08 19:31:50 +0000

    edit discussions view

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,8 +1,10 @@
 class DiscussionsController < ApplicationController
   def index
+    @discussions = Discussion.all
   end
 
   def show
+    @discussion = Discussion.find(params[:id])
   end
 
   def new
@@ -10,5 +12,30 @@ class DiscussionsController < ApplicationController
   end
 
   def edit
+    @discussion = Discussion.find(params[:id])
   end
+  
+  def update 
+    discussion = Discussion.find(params[:id])
+    discussion.update!(discussion_params)
+    redirect_to discussions_url, notice: "ディスカッションテーマ「#{discussion.theme}」を更新しました。"
+  end
+  
+　 def destroy
+　  discussion = Discussion.find(params[:id])
+　  discussion.destroy
+　  redirect_to discussions_url, notice: "ディスカッションテーマ「#{discussion.theme}」を削除しました。"
+   end
+  
+  def create
+    discussion = Discussion.new
+    discussion.save!
+    redirect_to discussions_url, notice: "ディスカッションテーマ「#{discussion.theme}」を登録しました。"
+  end
+  
+  private
+  def discussion_params
+    params.require(:discussion).permit(:theme, :discription)
+  end
+  
 end
